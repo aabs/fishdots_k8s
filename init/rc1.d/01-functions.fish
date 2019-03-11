6c86c85e3e95ff0dffdd544c9d1652e3a50212ea
Andrew Matthews <andrew.matthews@team.telstra.com.au>
2019-03-12 08:44:00 +1100

    more tinkering

--- init/rc1.d/01-functions.fish
+++ init/rc1.d/01-functions.fish
@@ -11,7 +11,10 @@ function vsparc
     case logs
         vsparc_logs $argv[2]
     case proxy
-        vsparc_proxy_pod $argv[2] $argv[3] $argv[4]
+        vsparc_proxy_pod $argv[2] $argv[3] $argv[4] $argv[5]
+
+    case tsdb
+        vsparc_proxy_tsdb
 
     case kmgr
         vsparc_proxy_kafkamgr
@@ -50,8 +53,12 @@ function vsparc_help -d "display usage info"
   echo "  get logs for selected pod"
   echo ""
 
-  echo "vsparc proxy <port here> <port there> <pod name>"
+  echo "vsparc proxy <pod name> <port here> <port there> <namespace>"
   echo "  creates a proxy from the local port to the remote port on the named pod"
+  echo ""
+
+  echo "vsparc tsdb"
+  echo "  creates a proxy to allow the use of timescale"
   echo ""
 
   echo "vsparc kmgr"
@@ -91,32 +98,51 @@ function vsparc_proxy_pod -a podname localport remoteport ns
 end
 
 
+function vsparc_proxy_tsdb
+  vsparc_proxy_pod "vsparc-timescale-0" 5432 5432 'vsparc'
+end
+
 function vsparc_proxy_k8s_dashboard
   set -l depname (k8s get deploy -n kube-system | grep "kubernetes-dashboard" | cut -d' ' -f 1)
   set -l podname (k8s get pod -n kube-system -o json | jq -r ".items[].metadata.name | select(contains(\"$depname\"))")
-  vsparc_proxy_pod "$podname" 9092 9090 'kube-system'
+  display_proxy_details "k8s_dashboard" 7010
+  vsparc_proxy_pod "$podname" 7010 9090 'kube-system'
 end
 
 function vsparc_proxy_kafkamgr
   set -l depname (k8s get deploy -n vsparc | grep "kafka-manager" | cut -d' ' -f 1)
   set -l podname (k8s get pod -n vsparc -o json | jq -r ".items[].metadata.name | select(contains(\"$depname\"))")
-  vsparc_proxy_pod "$podname" 16668 80 'vsparc'
+  display_proxy_details "kafka manager" 7011
+  vsparc_proxy_pod "$podname" 7011 80 'vsparc'
 end
 
 
 function vsparc_proxy_grafana
   set -l depname (k8s get deploy -n timescaledb | grep "grafana" | cut -d' ' -f 1)
   set -l podname (k8s get pod -n timescaledb -o json | jq -r ".items[].metadata.name | select(contains(\"$depname\"))")
-  vsparc_proxy_pod "$podname" 16669 3000 'timescaledb'
+  display_proxy_details "grafana" 7012
+  vsparc_proxy_pod "$podname" 7012 3000 'timescaledb'
 end
 
 function vsparc_proxy_pgadmin_dashboard
   set -l depname (k8s get deploy -n vsparc | grep "timescale-admin" | cut -d' ' -f 1)
   set -l podname (k8s get pod -n vsparc -o json | jq -r ".items[].metadata.name | select(contains(\"$depname\"))")
-  vsparc_proxy_pod "$podname" 16667 80 'vsparc'
+  display_proxy_details "pgadmin" 7012
+  vsparc_proxy_pod "$podname" 7013 80 'vsparc'
+end
+
+function vsparc_proxy_kafka
+  set -l depname (k8s get deploy -n prod-tlive-es | grep "kafka" | cut -d' ' -f 1)
+  set -l podname (k8s get pod -n vsparc -o json | jq -r ".items[].metadata.name | select(contains(\"$depname\"))")
+  display_proxy_details "kafka" 7013
+  vsparc_proxy_pod "$podname" 7013 80 'vsparc'
 end
 
 function vsparc_get_ls2ts_pod_name
   set -l podname (k8s get pod -n vsparc -o json | jq -r ".items[].metadata.name | select(contains(\"vsparc-tsdb-logstash-deployment\"))")
   echo $podname
+end
+
+function display_proxy_details -a name port
+  echo "proxying $name on port $port"
 end
